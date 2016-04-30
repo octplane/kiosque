e7aad082c424e2cf73d78af064aedf51843b49cb
Pierre Baillet <pierre@baillet.name>
2016-04-30 11:15:07 +0200

    Writer works

--- tests/lib.rs
+++ tests/lib.rs
@@ -20,7 +20,6 @@ use chrono::duration::Duration;
 use chrono::datetime::DateTime;
 use chrono::offset::utc::UTC;
 use chrono::Timelike;
-use std::fmt::Display;
 
 static APACHE_FORMAT:&'static str = "%d/%b/%Y:%H:%M:%S %z";
 static REFERERS: &'static [&'static str] = &["-","http://www.casualcyclist.com","http://bestcyclingreviews.com/top_online_shops","http://bleater.com","http://searchengine.com"];
@@ -43,70 +42,129 @@ pub fn line_generator(event_count: u32) -> Vec<(DateTime<UTC>, String)> {
   println!("Will generate {} events across {} seconds.", event_count, total_duration.num_seconds());
   let mut times = Vec::<i64>::with_capacity(event_count as usize);
 
-  for x in 1..event_count {
-    times.push(rng.gen_range(0,total_duration.num_seconds()) * 1000000 );
+  for _ in 1..event_count {
+    let _ = times.push(rng.gen_range(0,total_duration.num_seconds()) * 1000000 );
   }
   times.sort();
-  times.into_iter().map( |time| {
+  let o = times.into_iter().map( |time| {
     let r = REFERERS[rng.gen_range(0, REFERERS.len())];
     let ua = USERAGENTS[rng.gen_range(0, USERAGENTS.len())];
-    let mut uri = RESOURCES[rng.gen_range(0, RESOURCES.len())]; 
+    let uri = RESOURCES[rng.gen_range(0, RESOURCES.len())]; 
 
-    if let Some(_) = uri.find("Store") {
-      let uri = &format!("{}{}", uri, rng.gen_range(1000, 1500));
-    }
+    let prefix = match uri.find("Store") {
+      Some(_) => format!("{}{}", uri, rng.gen_range(1000, 1500)),
+      None => "".into()
+    };
 
     let (datetime, s_time) = apache_time(now, time);
-    let line = format!("{} - - [{}] \"GET {} HTTP/1.0\" 200 {} \"{}\" \"{}\"",
-                       random_ip(), s_time , uri, rng.gen_range(2000, 5000),
+    let line = format!("{} - - [{}] \"GET {}{} HTTP/1.0\" 200 {} \"{}\" \"{}\"",
+                       random_ip(), s_time , uri, prefix,  rng.gen_range(2000, 5000),
                        r, ua);
     (datetime, line)
-  }).collect()
+  }).collect();
+  println!("Done.");
+  return o;
 }
+
+
+use logformat::schema_capnp::{logblock, logline};
+pub fn append_line(ts: DateTime<UTC>, content: &str, lline: &mut logline::Builder)  {
+  let second_in_micro = ts.timestamp() as u64 * 1000000;
+  let us: u64 = (ts.nanosecond() / 1000) as u64; 
+  let timestamp_in_micro: u64  = ( second_in_micro + us) as u64;
+
+  lline.set_time(timestamp_in_micro);
+  lline.set_facility("Test Facility".into());
+  {
+    let mut facets_builder = lline.borrow().init_facets();
+    let mut kv = facets_builder.borrow().init_entries(1);
+
+    let _ = kv.borrow().get(0).set_key("stdout");
+    let _ = kv.borrow().get(0).set_value(content);
+  }
+}
+
+
+use capnp::message::Builder;
+use capnp::serialize_packed;
+
+use std::fs::File;
+
+
+pub fn build_log_block(file_suffix: &str, counter: u32, logblock_size: u32 )  {
+  let mut message = Builder::new_default();
+  {
+    let mut block = message.init_root::<logblock::Builder>();
+    let mut lines = block.borrow().init_entries(logblock_size);
+
+    for (ix, (ts, line)) in line_generator(logblock_size).into_iter().enumerate() {
+      let lines_ix: u32 = ix as u32;
+      let mut cline = lines.borrow().get(lines_ix);
+      append_line(ts, &line, &mut cline);
+    }
+  }
+  let file_name = format!("{}{}.capnp", file_suffix, counter);
+  let mut f = File::create(file_name).unwrap();
+  let _ = serialize_packed::write_message(&mut f, &message);
+}
+
+use std::io::{self, BufReader};
+
+// We derive `Debug` because all types should probably derive `Debug`.
+#[derive(Debug)]
+pub enum ReadError {
+    Io(io::Error),
+    Proto(capnp::Error),
+}
+
+impl From<io::Error> for ReadError {
+    fn from(err: io::Error) -> ReadError {
+        ReadError::Io(err)
+    }
+}
+
+impl From<capnp::Error> for ReadError {
+    fn from(err: capnp::Error) -> ReadError {
+        ReadError::Proto(err)
+    }
+}
+
+pub fn read_log_block(file_name: &str) -> Result<(), ReadError> {
+  let f = try!(File::open(file_name));
+  let mut bufreader = BufReader::new(f); 
+  let message_reader = try!(serialize_packed::read_message(
+      &mut bufreader,
+      ::capnp::message::ReaderOptions::new()));
+  let logblock = try!(message_reader.get_root::<logblock::Reader>());
+
+  for line_reader in try!(logblock.get_entries()).iter() {
+    let t = line_reader.get_time();
+    let f = line_reader.get_facility().unwrap();
+
+    println!("{:?} {:}",t, f);
+  }
+
+
+
+  Ok(())
+}
+
 
 
 
 #[cfg(test)]
 mod tests {
   use super::*;
-  use logformat::schema_capnp::{logblock};
-  use capnp::message::Builder;
-  use capnp::serialize;
-  use chrono::Timelike;
-
-  use std::collections::HashMap;
-
-
+  
   #[test]
   fn it_works() {
-
-    let mut message = Builder::new_default();
-    {
-      let logblock_sz = 500;
-      let mut block = message.init_root::<logblock::Builder>();
-      let mut lines = block.borrow().init_entries(logblock_sz);
-
-      for (ix, (ts, line)) in line_generator(logblock_sz).into_iter().enumerate() {
-        let lines_ix: u32 = ix as u32;
-        let second_in_micro = ts.timestamp() as u64 * 1000000;
-        let us: u64 = (ts.nanosecond() / 1000) as u64; 
-        let timestamp_in_micro: u64  = ( second_in_micro + us) as u64;
-        let sline: &str = &line;
-
-        let mut cline = lines.borrow().get(lines_ix);
-        cline.set_time(timestamp_in_micro);
-        cline.set_facility("Test Facility".into());
-        {
-          let mut facets_builder = cline.borrow().init_facets();
-          let mut kv = facets_builder.borrow().init_entries(1);
-
-          kv.borrow().get(0).set_key("stdout");
-          kv.borrow().get(0).set_value(sline);
-        }
-      }
+    for x in 0..1 {
+      build_log_block("sample", x, 5000);
     }
-    let mut vec = Vec::new();
-    serialize::write_message(&mut vec, &message);
-    assert!(vec.len() == 12, "vec len: {}", vec.len());
+
+    match read_log_block("sample0.capnp") {
+      Err(e) => println!("{:?}", e),
+      Ok(_) => {}
+    }
   }
 }
